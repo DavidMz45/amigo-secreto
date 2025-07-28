808ed64bad112f7f5f95837a7489c4170ae878ff
dmanzueta <dmanzueta@cesariglesias.com.do>
2025-07-28 11:33:36 -0400

    añadiendo funcion para actualizarLista

--- app.js
+++ app.js
@@ -12,8 +12,19 @@ function agregarAmigo() {
 
   amigos.push(amigoEntrada);
   limpiarEntrada();
+  actualizarLista();
 }
 
 function limpiarEntrada() {
   document.getElementById("amigo").value = "";
+}
+
+function actualizarLista() {
+  let lista = document.getElementById("listaAmigos");
+  lista.innerHTML = "";
+  for (let i = 0; i < amigos.length; i++) {
+    let li = document.createElement("li");
+    li.textContent = amigos[i];
+    lista.appendChild(li);
+  }
 }
