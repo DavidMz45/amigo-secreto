199fa701a3e3bbdafea0644ced03788d6c958853
dmanzueta <dmanzueta@cesariglesias.com.do>
2025-07-28 11:22:19 -0400

    Añadiendo funcion para obtener entrada

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+var amigos = [];
+
+function agregarAmigo() {
+  let amigoEntrada = document.getElementById("amigo").value;
+
+  if (amigoEntrada === "") {
+    alert("Por favor, inserte un nombre.");
+    return;
+  }
+
+  amigos.push(amigoEntrada);
+  limpiarEntrada();
+}
+
+function limpiarEntrada() {
+  document.getElementById("amigo").value = "";
+}
